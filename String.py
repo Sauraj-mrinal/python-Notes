a9153b1eb7ee1d95b0fc7d9217ec7f1539658636
Sauraj-mrinal <msauraji@gmail.com>
2024-01-18 16:27:45 +0530

    Add

--- String.py
+++ String.py
@@ -1,3 +1,56 @@
+#:-----------------------------------------------------------------------------------------------------------------------------------------------
+
+
 # String is sequence of charactor 
 
-# String can be created 
+# String can be created by enclosing characters inside a single quote or double quotes . Evwn triple Quotes can be used in python but 
+# generally used to repersent multiline string and docstring 
+
+
+# Strings are immutable . This means that element of a string cannot be change once it has been created 
+# We can simply re assign different string to the same name .
+
+
+name  = " Mrinal ";
+
+# to write paragraph we use ''' any thing we can write here  '''
+
+
+paragraph  = ''' this is a paragraph
+ and i want to print it '''
+
+print(paragraph)
+print(name )
+
+# same we can Access string through index 
+
+fruit  = " apple";
+print(fruit[0])
+print(fruit[1])
+print(fruit[2])
+print(fruit[3])
+
+# for  better notes go on 
+# https://www.w3schools.com/python/python_strings.asp
+
+# https://www.geeksforgeeks.org/python-string/?ref=lbp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
